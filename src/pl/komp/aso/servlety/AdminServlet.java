15b939f12bff1903cb6909ca36ce363ddd9270fa
iKatha <katha.ftw@gmail.com>
2017-04-03 15:16:35 +0200

    commit Kasia

--- src/pl/komp/aso/servlety/AdminServlet.java
+++ src/pl/komp/aso/servlety/AdminServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import pl.komp.aso.sterowniki.SterownikPolBD;
 
-
 public class AdminServlet extends HttpServlet {
 
 //	protected void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
