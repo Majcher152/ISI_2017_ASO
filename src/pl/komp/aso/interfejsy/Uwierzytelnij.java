24686ede6edf2a0d86461aa63306fca14427b954
Łukasz Majcher <lukasz.majcherrr@gmail.com>
2017-05-30 02:49:28 +0200

    Dodany adapter, wzorzec projektowy

---
+++ src/pl/komp/aso/interfejsy/Uwierzytelnij.java
@@ -0,0 +1,7 @@
+package pl.komp.aso.interfejsy;
+
+public interface Uwierzytelnij {
+
+	public int uwierzytelnij(String imie, String nazwisko, String email, String numer_telefonu, String login,
+			String noweHaslo, String noweHaslo2);
+}
